9215025cf9b7daa8fad3ef7345c7784e03984859
jabezinu <jabezzinu31@gmail.com>
2025-11-17 09:43:06 +0300

    remove comment page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,8 +2,6 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import GemstonesList from './components/GemstonesList'
 import GemstoneForm from './components/GemstoneForm'
-import CommentsList from './components/CommentsList'
-import CommentForm from './components/CommentForm'
 import CoursesList from './components/CoursesList'
 import CourseForm from './components/CourseForm'
 import EquipmentsList from './components/EquipmentsList'
@@ -17,9 +15,6 @@ export default function App() {
         <Route path="/" element={<GemstonesList />} />
         <Route path="/gemstones/new" element={<GemstoneForm />} />
         <Route path="/gemstones/:id/edit" element={<GemstoneForm />} />
-        <Route path="/comments" element={<CommentsList />} />
-        <Route path="/comments/new" element={<CommentForm />} />
-        <Route path="/comments/:id/edit" element={<CommentForm />} />
         <Route path="/courses" element={<CoursesList />} />
         <Route path="/courses/new" element={<CourseForm />} />
         <Route path="/courses/:id/edit" element={<CourseForm />} />
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import 'dotenv/config';
 import gemstonesRouter from './routes/gemstones.js';
-import commentsRouter from './routes/comments.js';
 import coursesRouter from './routes/courses.js';
 import equipmentsRouter from './routes/equipments.js';
 import contactMessagesRouter from './routes/contactMessages.js';
@@ -20,10 +19,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
-  credentials: true
-}));
+app.use(cors({}));
 app.use(express.json());
 
 // MongoDB connection
@@ -33,7 +29,6 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // Routes
 app.use('/api/gemstones', gemstonesRouter);
-app.use('/api/comments', commentsRouter);
 app.use('/api/courses', coursesRouter);
 app.use('/api/equipments', equipmentsRouter);
 app.use('/api/contact-messages', contactMessagesRouter);
